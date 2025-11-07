44fbbddd432b7126fdf0678efd53ddd061358d88
kitty667-w <krodriguezs@utem.cl>
2025-11-07 04:29:41 -0300

    Update Screen1.scm

--- src/appinventor/usuario/mi_tiempo_diario/Screen1.scm
+++ src/appinventor/usuario/mi_tiempo_diario/Screen1.scm
@@ -2,21 +2,11 @@
 $JSON
 {"YaVersion":5,"Properties":{
 "$Name":"Screen1",
-"AppName":"Mi_Tiempo_Diario",
-"Title":"Pantalla inicial",
+"Title":"Prueba",
 "Sizing":"Responsive",
-"ShowListsAsJson":true,
 "Scrollable":false,
-"AlignHorizontal":3,
-"AlignVertical":2,
-"BackgroundColor":"&HFF1B4D3E",
 "Theme":"Classic",
 "Components":[
-  {"$Name":"ImagenLogo","$Type":"Image","Properties":{
-    "Height":200,"Width":200,"Picture":"splash_icon.png","Visible":true}},
-  {"$Name":"EtiquetaTitulo","$Type":"Label","Properties":{
-    "Text":"Mi Tiempo Diario","FontBold":true,"FontSize":26,"TextColor":"&HFFF7E8C9","TextAlignment":1}},
-  {"$Name":"BtnEntrar","$Type":"Button","Properties":{
-    "Text":"Continuar","FontBold":true,"FontSize":18,"TextColor":"&HFFFFFFFF","BackgroundColor":"&HFF2BB673","Width":200}}
+  {"$Name":"Etiqueta1","$Type":"Label","Properties":{"Text":"Hola mundo"}}
 ]}}
 |#
