19021740ce3eeb3404e3799afc72062f7b221b00
kitty667-w <krodriguezs@utem.cl>
2025-11-07 04:21:47 -0300

    Update user input screen properties and labels

--- src/appinventor/usuario/mi_tiempo_diario/Inicio.scm
+++ src/appinventor/usuario/mi_tiempo_diario/Inicio.scm
@@ -1,46 +1,23 @@
 #|
 $JSON
-{"YaVersion":5,"Properties":
-{"$Name":"Inicio",
-"Title":"Ingreso de usuario",
-"AppName":"Mi_Tiempo_Diario",
+{"YaVersion":5,"Properties":{
+"$Name":"Inicio",
+"Title":"Ingresar nombre",
+"Sizing":"Responsive",
+"ShowListsAsJson":true,
 "Scrollable":false,
-"BackgroundColor":"&HFFF7E8C9",
 "AlignHorizontal":3,
 "AlignVertical":2,
-"Sizing":"Responsive",
+"BackgroundColor":"&HFFF7E8C9",
 "Theme":"Classic",
 "Components":[
-{"$Name":"EtiquetaTitulo","$Type":"Label","Properties":{
-  "Text":"Bienvenido a Mi Tiempo Diario",
-  "FontBold":true,
-  "FontSize":24,
-  "TextColor":"&HFF1B4D3E",
-  "TextAlignment":1
-}},
-{"$Name":"EtiquetaNombre","$Type":"Label","Properties":{
-  "Text":"Ingrese su nombre:",
-  "FontSize":18,
-  "TextAlignment":1
-}},
-{"$Name":"CampoNombre","$Type":"TextBox","Properties":{
-  "Hint":"Escriba su nombre aquí",
-  "FontSize":16,
-  "TextAlignment":1,
-  "Width":"250"
-}},
-{"$Name":"BotonContinuar","$Type":"Button","Properties":{
-  "Text":"Continuar",
-  "FontBold":true,
-  "FontSize":18,
-  "TextColor":"&HFFFFFFFF",
-  "BackgroundColor":"&HFF1B4D3E",
-  "Width":"150"
-}},
-{"$Name":"Notificacion","$Type":"Notifier","Properties":{}},
-{"$Name":"BaseDatos","$Type":"TinyDB","Properties":{
-  "Name":"TinyDB1"
-}}
-]
-}}
+  {"$Name":"Etiqueta1","$Type":"Label","Properties":{
+    "Text":"¿Cómo te llamas?","FontSize":20,"FontBold":true,"TextColor":"&HFF1B4D3E","TextAlignment":1}},
+  {"$Name":"CampoNombre","$Type":"TextBox","Properties":{
+    "Hint":"Escribe tu nombre (min 3 letras)","FontSize":16,"Width":260}},
+  {"$Name":"BtnContinuar","$Type":"Button","Properties":{
+    "Text":"Guardar y continuar","FontBold":true,"FontSize":18,"TextColor":"&HFFFFFFFF","BackgroundColor":"&HFF1B4D3E","Width":240}},
+  {"$Name":"Aviso","$Type":"Notifier","Properties":{}},
+  {"$Name":"DB","$Type":"TinyDB","Properties":{"Namespace":"MiTiempoDiario"}}
+]}}
 |#
